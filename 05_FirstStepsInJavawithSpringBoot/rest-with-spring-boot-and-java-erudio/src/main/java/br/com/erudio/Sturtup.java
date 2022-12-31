ec41ec55409526ed3c0c1865a3daa9be6e9df11a
Ricardo Pontes <00119110450@pq.uenf.br>
2022-12-31 00:06:50 -0300

    First steps in java with Spring Boot

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Sturtup.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sturtup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sturtup.class, args);
+	}
+
+}
