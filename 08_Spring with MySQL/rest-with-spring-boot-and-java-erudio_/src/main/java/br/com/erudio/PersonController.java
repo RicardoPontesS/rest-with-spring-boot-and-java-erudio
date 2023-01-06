4df05bfe2b9e12babc19fbb6421fcf0d1c2ecb92
Ricardo Pontes <00119110450@pq.uenf.br>
2023-01-06 12:18:52 -0300

    Update

--- 08_Spring with MySQL/rest-with-spring-boot-and-java-erudio_/src/main/java/br/com/erudio/PersonController.java
+++ 08_Spring with MySQL/rest-with-spring-boot-and-java-erudio_/src/main/java/br/com/erudio/PersonController.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,29 +30,27 @@ public class PersonController {
 
 	// chamada do método (requisição) vinda do navegador
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
-
+	@PostMapping(value = "/{id}")
 	public Person create(@RequestBody() Person person) throws Exception {
 
-		return service.create(person);   
-    
+		return service.create(person);
+
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-
+	@PutMapping(value = "/{id}")
 	public Person update(@RequestBody() Person person) throws Exception {
 
 		return service.update(person);
 
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable(value ="id") Long id) {
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable(value = "id") Long id) {
 
 		service.delete(id);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/{id}")
 
 	public Person findById(@PathVariable(value = "id") Long id) throws Exception {
 
